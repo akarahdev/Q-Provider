2972505c1e7a528a83494ca5483c250d2cdf8b23
akarahdev <endisticakarah@gmail.com>
2024-09-26 09:58:36 -0400

    Add new criteria-based event system

--- src/main/java/dev/akarah/mixin/criteria/PlayerTriggerMixin.java
+++ src/main/java/dev/akarah/mixin/criteria/PlayerTriggerMixin.java
@@ -1,5 +1,10 @@
 package dev.akarah.mixin.criteria;
 
+import dev.akarah.APIProvider;
+import dev.akarah.datatypes.server.Identifier;
+import dev.akarah.events.components.EventData;
+import dev.akarah.provider.entity.EntityImpl;
+import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.advancements.critereon.PlayerTrigger;
 import net.minecraft.server.level.ServerPlayer;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,6 +19,19 @@ public class PlayerTriggerMixin {
             at = @At("HEAD")
     )
     public void trigger(ServerPlayer player, CallbackInfo ci) {
-
+        if((Object) this == CriteriaTriggers.LOCATION) {
+            APIProvider.dispatchEvent(
+                    Identifier.of("minecraft:location"),
+                    EventData.Builder.empty()
+                            .mainEntity(new EntityImpl(player))
+            );
+        }
+        if((Object) this == CriteriaTriggers.TICK) {
+            APIProvider.dispatchEvent(
+                    Identifier.of("minecraft:tick"),
+                    EventData.Builder.empty()
+                            .mainEntity(new EntityImpl(player))
+            );
+        }
     }
 }
